6bab3848dc86c1cc1937b2ea930c21b7c5d7c751
Dawid Przydryga <shivva90@gmail.com>
2020-11-16 11:53:48 +0100

    Add addMessage function

--- client/app.js
+++ client/app.js
@@ -1,17 +1,18 @@
-const loginForm = document.getElementById('#welcome-form');
-const messagesSection = document.getElementById('#message-section');
-const messageList = document.getElementById('#message-list');
-const addMessageForm = document.getElementById('#add-messages-form');
-const userNameInput = document.getElementById('#username');
-const messageContentInput = document.getElementById('#message-content');
+const loginForm = document.getElementById('welcome-form');
+const messagesSection = document.getElementById('messages-section');
+const messagesList = document.getElementById('messages-list');
+const addMessageForm = document.getElementById('add-messages-form');
+const userNameInput = document.getElementById('username');
+let messageContentInput = document.getElementById('message-content');
 
 let userName;
+
 
 const login = (e) => {
     e.preventDefault();
 
     if (userNameInput.value == '') {
-        window.alert('Please write your login')
+        window.alert('Please write your login');
     } else {
         userName = userNameInput.value;
         loginForm.classList.remove('show');
@@ -19,4 +20,30 @@ const login = (e) => {
     }
 };
 
-loginForm.addEventListener('submit', login)
+function addMessage(author, content) {
+    const message = document.createElement('li');
+    message.classList.add('message');
+    message.classList.add('message--received');
+    if(author === userName) message.classList.add('message--self');
+    message.innerHTML = `
+      <h3 class="message__author">${userName === author ? 'You' : author }</h3>
+      <div class="message__content">
+        ${content}
+      </div>
+    `;
+    messagesList.appendChild(message);
+  }
+
+const sendMessage = (e) => {
+    e.preventDefault();
+
+    if (messageContentInput.value == '') {
+        window.alert('Add your message')
+    } else {
+        addMessage(userName, messageContentInput.value);
+        messageContentInput.value = '';
+    }
+};
+
+loginForm.addEventListener('submit', login);
+addMessageForm.addEventListener('submit', sendMessage);
