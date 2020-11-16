6cf03e90a1f891c22406adf77debdde62c9ced18
Dawid Przydryga <shivva90@gmail.com>
2020-11-16 11:32:11 +0100

    Add login validation

--- client/app.js
+++ client/app.js
@@ -4,3 +4,19 @@ const messageList = document.getElementById('#message-list');
 const addMessageForm = document.getElementById('#add-messages-form');
 const userNameInput = document.getElementById('#username');
 const messageContentInput = document.getElementById('#message-content');
+
+let userName;
+
+const login = (e) => {
+    e.preventDefault();
+
+    if (userNameInput.value == '') {
+        window.alert('Please write your login')
+    } else {
+        userName = userNameInput.value;
+        loginForm.classList.remove('show');
+        messagesSection.classList.add('show');
+    }
+};
+
+loginForm.addEventListener('submit', login)
