3e8b6e5ab122c377aae539c78aad40635839f8ae
Dawid Przydryga <shivva90@gmail.com>
2020-11-16 11:23:32 +0100

    Add ref to html file

---
+++ client/app.js
@@ -0,0 +1,6 @@
+const loginForm = document.getElementById('#welcome-form');
+const messagesSection = document.getElementById('#message-section');
+const messageList = document.getElementById('#message-list');
+const addMessageForm = document.getElementById('#add-messages-form');
+const userNameInput = document.getElementById('#username');
+const messageContentInput = document.getElementById('#message-content');
